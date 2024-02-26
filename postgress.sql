3e6aa4418ae37698a0370d05969d71d18e0c0690
IgorVechirko <vechirkoigor@gmail.com>
2024-02-26 23:33:33 +0200

    Update postgress.sql

--- postgress.sql
+++ postgress.sql
@@ -13,3 +13,23 @@ WHERE remote_ration = 100
 		AND company_location = 'US'
 ORDER BY salary_in_usd DESC
 LIMIT 10;
+
+
+--Count and display amount off all rows
+SELECT 
+	COUNT(*) AS amount
+FROM salaries
+LIMIT 10;
+
+
+--No empty(null) values in column exp_level
+SELECT 
+	CASE COUNT(*) as total
+	, COUNT(exp_level) as expirience
+FROM salaries
+LIMIT 10;
+--or
+SELECT 
+	(CASE WHEN COUNT(*) = COUNT(exp_level) THEN 1 ELSE 0 END) AS no_empty_exp_level
+FROM salaries
+LIMIT 10;
