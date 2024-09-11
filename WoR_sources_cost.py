81b843a8a2ed433ce427304bdcc7d93fdf650bee
IgorVechirko <vechirkoigor@gmail.com>
2024-09-11 14:01:03 +0300

    Update WoR_sources_cost.py

--- WoR_sources_cost.py
+++ WoR_sources_cost.py
@@ -23,7 +23,9 @@ exp_raid_drop = (16, {Item("hero", "rare"): 1, Item("hero", "magic"): 11, Item("
 exp_raid_drop = (41, {Item("hero", "rare"): 1, Item("hero", "magic"): 23, Item("hero", "common"): 22, Item("gold"): 37750})
 exp_raid_drop = (29, {Item("hero", "rare"): 1, Item("hero", "magic"): 21, Item("hero", "common"): 15, Item("gold"): 21750})
 
-marksmen_insignia_15 = (10, {Item("gold"): 7800, Item("exp"): 136500, Item("hero", "common"): 15, Item("insignia_marksman", "III): 74})
+marksmen_insignia_15 = (10, {Item("gold", None, x2): 7800, Item("exp"): 136500, Item("insignia_marksman", "III): 74})
+marksmen_insignia_15 = (6, {Item("gold", None, "x2): 9360, Item("exp"): 8190, Item("insignia_marksman", "III): 78, Item("extract", "mythic"): 1})   
+                                                                                                       
 
 class Raid:
     def __init__(self, name, stamina):
